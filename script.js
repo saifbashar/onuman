9f8ed2b16a98a8d70598f6e9e0d8d3c37547f083
Saif Bashar <60818905+saifbashar@users.noreply.github.com>
2021-04-13 22:18:40 +0600

    Project file added

---
+++ script.js
@@ -0,0 +1,90 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 21;
+document.querySelector('.score').textContent = 30;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 20;
+console.log(document.querySelector('.guess').value);
+*/
+/*
+let name = 'Saif Bashar';
+console.log(name);
+let speed = 50;
+let k = 0;
+
+function typeWritter() {
+  if (k < name.length) {
+    document.getElementById('.demo').innerHTML += name.charAt(i);
+    k++;
+    setTimeout(typeWritter, speed);
+  }
+}
+typeWritter();
+*/
+
+let highScore = 0;
+let randomNumber = Math.trunc(Math.random() * 100) + 1;
+console.log(randomNumber);
+let score = 20;
+// console.log(randomNumber);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //! when player doesn't input a number
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number 😴';
+  }
+  //! when players win the game
+  else if (guess === randomNumber) {
+    document.querySelector('.message').textContent = 'Currect Number😎';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = randomNumber;
+  }
+
+  //! when number is too high
+  else if (guess > randomNumber) {
+    document.querySelector('.message').textContent = 'Too High ☹';
+    document.querySelector('body').style.backgroundColor = 'red';
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose the Gamne';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //! when number is too low
+  else if (guess < randomNumber) {
+    document.querySelector('.message').textContent = 'Too Low 🦄';
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You loss the game😿';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = '20';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  randomNumber = Math.trunc(Math.random() * 100) + 1;
+  console.log(randomNumber);
+  if (score > highScore) {
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+  }
+
+  score = 20;
+  document.querySelector('.message').textContent = 'Start Guessing...';
+});
